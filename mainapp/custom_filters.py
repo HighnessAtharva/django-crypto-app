66194b5b71158f0d60d37f0dd40e5c4c7b2700b5
HighnessAtharva <HighnessAtharva@gmail.com>
2023-05-01 23:20:30 +0530

    truncate unecessary files

--- mainapp/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter(name='attr')
-def attr(ob, val):
-    try:
-        return ob[int(val)]
-    except:
-        return getattr(ob, val)
